3fc7e9a106e0a1e119977605c038c83344d1fa28
Jimboom7 <jimboom7@yahoo.de>
2024-03-20 22:40:56 +0100

    Update testcase

--- evaluate.py
+++ evaluate.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
     NUM_GAMES = 10000
-    MODEL = "skat_27_final"
-    FRAMES = "690"
+    MODEL = "skat_28_final_fixed"
+    FRAMES = "70"
     BLIND_HAND_CHANCE = 0.1
     NUM_ACTORS = 12
 
--- test.py
+++ test.py
@@ -36,14 +36,48 @@ def case2(raw_state):
     raw_state['bid_jacks'] = [0, 2, 0]
     return raw_state
 
+def case3(raw_state):
+    raw_state['trace'] = [(0, "SK")]
+    raw_state['played_cards'] = [["SK"],[],[]]
+    raw_state['self'] = 1
+    raw_state['current_hand'] = ["DJ", "HJ", "S7", "S8", "S9", "ST", "HQ", "HK", "CA", "C8"]
+    raw_state['others_hand'] = ["SJ", "D7", "D8", "D9", "DQ", "DK", "DT", "DA", "H7", "H8", "H9", "HT", "HA", "SQ", "CJ", "C7", "C9", "CQ", "CT", 'CK', 'SA']
+    raw_state['points'] = [0, 0]
+    raw_state['actions'] = ["S7", "S8", "S9", "ST"]
+    raw_state['trick'] = [(0, "SK")]
+    raw_state['blind_hand'] = False
+    raw_state['skat'] = []
+    raw_state['bids'] = [{'D': 0, 'H': 0, 'S': 0, 'C': 0, 'N': 0},
+                    {'D': 0, 'H': 0, 'S': 0, 'C': 0, 'N': 0},
+                    {'D': 1, 'H': 0, 'S': 0, 'C': 0, 'N': 0}]
+    raw_state['bid_jacks'] = [0, 0, 2]
+    return raw_state
+
+def case4(raw_state):
+    raw_state['trace'] = [(1, "HT"), (2, "HA"), (0, "DT"), (0, "DJ"), (1, "SJ"), (2, "DK"), (1, "H8"), (2, "HQ"), (0, "C7"), (2, "CA"), (0, "DQ"), (1, "C9"), (0, "HJ"), (1, "CQ"), (2, "D7")]
+    raw_state['played_cards'] = [["CT", "DJ", "D7", "CQ", "HJ"],["HT", "SJ", "H8", "D9", "DQ"],["HA", "CK", "HQ", "DA", "C7"]]
+    raw_state['self'] = 0
+    raw_state['current_hand'] = ["CJ", "DA", "D8", "SA", "S8"]
+    raw_state['others_hand'] = ["C8", "CK", "CT", "H9", "S7", "S9", "SQ", "SK", "ST", "D8", "D9"]
+    raw_state['points'] = [50, 11]
+    raw_state['actions'] = ["CJ", "DA", "D8", "SA", "S8"]
+    raw_state['trick'] = []
+    raw_state['blind_hand'] = False
+    raw_state['skat'] = ["H7", "HK"]
+    raw_state['bids'] = [{'D': 0, 'H': 0, 'S': 0, 'C': 0, 'N': 0},
+                    {'D': 0, 'H': 0, 'S': 0, 'C': 1, 'N': 0},
+                    {'D': 0, 'H': 1, 'S': 0, 'C': 0, 'N': 0}]
+    raw_state['bid_jacks'] = [0, 1, 1]
+    return raw_state
+
 if __name__ == '__main__':
 
     CHECKPOINT_DIR = "checkpoints/skat_27_final"
     FRAMES = "550"
 
-    MODEL1 = CHECKPOINT_DIR + "/2_" + FRAMES + ".pth"
-    MODEL2 = CHECKPOINT_DIR + "/2_" + FRAMES + ".pth"
-    MODEL3 = CHECKPOINT_DIR + "/2_" + FRAMES + ".pth"
+    MODEL1 = CHECKPOINT_DIR + "/0_" + FRAMES + ".pth"
+    MODEL2 = CHECKPOINT_DIR + "/0_" + FRAMES + ".pth"
+    MODEL3 = CHECKPOINT_DIR + "/0_" + FRAMES + ".pth"
 
     models = [
             MODEL1,
@@ -65,7 +99,7 @@ if __name__ == '__main__':
     #raw_state = case1(raw_state)
 
     #Testcase for Javascript Comparison
-    raw_state = case2(raw_state)
+    raw_state = case4(raw_state)
 
     state = env.extract_state(raw_state)
 
@@ -74,6 +108,6 @@ if __name__ == '__main__':
         for obs in state['obs']:
             the_file.write('    ' + str(obs) + ',\n')
 
-    _, info = agents[1].eval_step(state)
+    _, info = agents[0].eval_step(state)
 
     print(info)
