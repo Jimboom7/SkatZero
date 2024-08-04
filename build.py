f2a69308366dc05a1e13547c6a8cd32199b510bc
Jimboom7 <jimboom7@yahoo.de>
2024-08-04 20:20:32 +0200

    Update latest models

--- build.py
+++ build.py
@@ -31,10 +31,10 @@ def convert_onnx(player_id, input_size, gametype, postfix):
 if __name__ == "__main__":
 
     for i, x in enumerate([551, 573, 573]):
-        convert_onnx(i, x, 'D', '9200')
+        convert_onnx(i, x, 'D', '11790')
 
     for i, x in enumerate([551, 573, 573]):
         convert_onnx(i, x, 'G', '9980')
 
     for i, x in enumerate([310, 364, 364]):
-        convert_onnx(i, x, 'N', '3000')
+        convert_onnx(i, x, 'N', '4060')
