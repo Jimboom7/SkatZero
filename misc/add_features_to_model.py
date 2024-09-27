349c3bf4c0c155aa92d111aba6cfc8ac6d37506e
Jimboom7 <jimboom7@yahoo.de>
2024-09-27 14:04:36 +0200

    Model transformation

--- misc/add_features_to_model.py
+++ misc/add_features_to_model.py
@@ -11,21 +11,30 @@ import torch
 
 # new_state_dict = model.state_dict()
 
-model_path = 'models/checkpoints/skat_lstm_G/model.tar'
-model_path_new = 'models/checkpoints/skat_lstm_G/model_new.tar'
+model_path = 'models/checkpoints/skat_lstm_D/model.tar'
+model_path_2 = 'models/checkpoints/skat_resnet_D/model.tar'
+model_path_new = 'models/checkpoints/skat_resnet_D/model_new.tar'
 
 all_models = torch.load(model_path)
+all_models_2 = torch.load(model_path_2)
 
 # all_models['model_state_dict'][0]['fc_layers.0.weight']
 # all_models['optimizer_state_dict'][0]['state'][0]['square_avg']
 
-new_weights = torch.cat((all_models['model_state_dict'][0]['fc_layers.0.weight'][:,:-57], torch.zeros((512, 4), device='cpu'), all_models['model_state_dict'][0]['fc_layers.0.weight'][:,-57:]), dim=1)
+for i in range(0,3):
+    new_weights = torch.cat((torch.zeros((512, 384), device='cuda:0'), all_models['model_state_dict'][i]['fc_layers.0.weight'][:,:]), dim=1)
 
-all_models['model_state_dict'][0]['fc_layers.0.weight'] = new_weights
+    all_models_2['model_state_dict'][i]['fc_layers.0.weight'] = new_weights
+    all_models_2['model_state_dict'][i]['fc_layers.0.bias'] = all_models['model_state_dict'][i]['fc_layers.0.bias']
+    for j in range(2, 12, 2):
+        all_models_2['model_state_dict'][i]['fc_layers.' + str(j) + '.weight'] = all_models['model_state_dict'][i]['fc_layers.' + str(j) + '.weight']
+        all_models_2['model_state_dict'][i]['fc_layers.' + str(j) + '.bias'] = all_models['model_state_dict'][i]['fc_layers.' + str(j) + '.bias']
 
-state = all_models['optimizer_state_dict'][0]['state'][0]
-new_opt = torch.cat((state['square_avg'][:,:-57], torch.zeros((512, 4), device='cpu') + state['square_avg'].mean(), state['square_avg'][:,-57:]), dim=1)
-state['square_avg'] = new_opt
+    state = all_models['optimizer_state_dict'][i]['state'][0]
+    new_opt = torch.cat((torch.zeros((512, 384), device='cuda:0') + state['square_avg'].mean(), state['square_avg'][:,:]), dim=1)
+    all_models_2['optimizer_state_dict'][i]['state'][16]['square_avg'] = new_opt
+    for j in range(17, 28):
+        all_models_2['optimizer_state_dict'][i]['state'][j]['square_avg'] = all_models['optimizer_state_dict'][i]['state'][j - 16]['square_avg']
 
 
-torch.save(all_models, model_path_new)
+torch.save(all_models_2, model_path_new)
